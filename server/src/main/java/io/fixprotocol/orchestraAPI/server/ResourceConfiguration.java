b1cf4487a746671d2f86400dd42ae1adb2427ff4
Don Mendelson <donmendelson@gmail.com>
2018-04-19 16:56:09 -0500

    Correct incompatible library versions in generated project

--- server/src/main/java/io/fixprotocol/orchestraAPI/server/ResourceConfiguration.java
+++ server/src/main/java/io/fixprotocol/orchestraAPI/server/ResourceConfiguration.java
@@ -1,17 +1,16 @@
 package io.fixprotocol.orchestraAPI.server;
 
-import java.util.logging.Logger;
-
-import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class ResourceConfiguration extends ResourceConfig {
 
   public ResourceConfiguration() {
-    //packages("io.fixprotocol.orchestraAPI.server");
-   register(io.fixprotocol.orchestraAPI.server.RepositoriesApi.class);
+    // packages("io.fixprotocol.orchestraAPI.server");
+    register(io.fixprotocol.orchestraAPI.server.RepositoriesApi.class);
 
-    // Register an instance of LoggingFilter.
-    register(new LoggingFilter(Logger.getLogger(ResourceConfig.class.getName()), true));
+    // Register an instance of LoggingFeature.
+    /*register(new LoggingFeature(Logger.getLogger(getClass().getName()), Level.FINE,
+        LoggingFeature.Verbosity.PAYLOAD_ANY, Integer.MAX_VALUE));
+        */
   }
 }
